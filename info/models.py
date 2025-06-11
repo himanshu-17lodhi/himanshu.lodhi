ee808e98212181fcd19f6424300b735b25eb4726
Himanshu Lodhi <125793095+himanshu-17lodhi@users.noreply.github.com>
2025-06-11 23:39:21 +0530

    Update models.py

--- info/models.py
+++ info/models.py
@@ -26,43 +26,13 @@ class Information(models.Model):
     def __str__(self):
         return self.name_complete
 
-def validate_image_file_extension(value):
-    valid_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.svg']
-    ext = os.path.splitext(value.name)[1].lower()
-    if ext not in valid_extensions:
-        raise ValidationError('Unsupported file extension. Allowed: jpg, jpeg, png, gif, svg.')
-
-def competence_image_choices():
-    """
-    Returns a list of tuples for available images in static/competence/.
-    """
-    static_folder = os.path.join(settings.BASE_DIR, 'static', 'competence')
-    try:
-        files = [
-            (f'competence/{f}', f)
-            for f in os.listdir(static_folder)
-            if os.path.isfile(os.path.join(static_folder, f))
-        ]
-    except FileNotFoundError:
-        files = []
-    return files
-
 class Competence(models.Model):
     title = models.CharField(max_length=50, blank=False, null=False)
     description = models.TextField(blank=False, null=False)
-    image_from_static = models.CharField(
-        max_length=255,
-        blank=True,         
-        null=True,          
-        choices=competence_image_choices(),
-        verbose_name="Select image from static/competence/"
-    )
+    image = CloudinaryField('image',blank=False, null=False)
 
     def __str__(self):
         return self.title
-
-    # In your template, use: {% load static %} <img src="{% static competence.image_from_static %}" alt="{{ competence.title }}">
-    # There is no need for a get_image_url method.
 
 class Education(models.Model):
     title = models.CharField(max_length=50, blank=False, null=False)
@@ -84,7 +54,7 @@ class Project(models.Model):
     title = models.CharField(max_length=200, blank=False, null=False)
     slug = models.SlugField(max_length=200, blank=True, null=True)
     description = CKEditor5Field(blank=False, null=False)
-    image = models.ImageField(upload_to="projects/", blank=False, null=False)
+    image = CloudinaryField('image',blank=False, null=False)
     tools = models.CharField(max_length=200, blank=False, null=False)
     demo = models.URLField()
     github = models.URLField()
