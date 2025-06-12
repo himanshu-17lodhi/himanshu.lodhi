7a1f90326e5fc9e3cdb7643eac1a038e947bce57
Himanshu Lodhi <125793095+himanshu-17lodhi@users.noreply.github.com>
2025-06-12 15:20:53 +0530

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
     'info',
     'dashboard',
     'cloudinary_storage',
