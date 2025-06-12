96ea03d24b5752d03cffdafdf60f181cbacc4824
Himanshu Lodhi <125793095+himanshu-17lodhi@users.noreply.github.com>
2025-06-12 15:28:10 +0530

    Update create_admin.py

--- app/management/commands/create_admin.py
+++ app/management/commands/create_admin.py
@@ -1,19 +1,16 @@
-import os
-import django
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
-django.setup()
-
+from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
+class Command(BaseCommand):
+    help = 'Creates a superuser if it does not exist'
 
-username = "himanshu_lodhi"
-email = "lodhihimanshu17@gmail.com"
-password = "Jairajputana@17"
-
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
-    print("Superuser created!")
-else:
-    print("Superuser already exists.")
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        username = "himanshu_lodhi"
+        email = "lodhihimanshu17@gmail.com"
+        password = "Jairajputana@17" 
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username=username, email=email, password=password)
+            self.stdout.write(self.style.SUCCESS('Superuser created!'))
+        else:
+            self.stdout.write(self.style.WARNING('Superuser already exists.'))
