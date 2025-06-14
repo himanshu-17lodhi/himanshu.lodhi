467b8a53882a015e084fc5fd7160a0c2965f1d67
Himanshu Lodhi <125793095+himanshu-17lodhi@users.noreply.github.com>
2025-06-14 10:50:31 +0530

    added slugs_error

---
+++ app/management/commands/fix_projects_slugs.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from info.models import Project
+
+class Command(BaseCommand):
+    help = 'Fix project slugs that are None or empty'
+
+    def handle(self, *args, **kwargs):
+        projects = Project.objects.filter(slug__isnull=True) | Project.objects.filter(slug='')
+        
+        for project in projects:
+            old_slug = project.slug
+            project.slug = None  # Reset to trigger slug generation
+            project.save()
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f'Updated project "{project.title}": {old_slug} -> {project.slug}'
+                )
+            )
+        
+        self.stdout.write(
+            self.style.SUCCESS(f'Successfully updated {projects.count()} projects')
+        )
