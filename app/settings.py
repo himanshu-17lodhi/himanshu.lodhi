8c75e253d7260903c534d38f4619033532c45593
Himanshu Lodhi <125793095+himanshu-17lodhi@users.noreply.github.com>
2025-06-14 11:17:59 +0530

    updated CKEDITOR_5_CONFIGS

--- app/settings.py
+++ app/settings.py
@@ -89,10 +89,13 @@ cloudinary.config(
 CKEDITOR_5_CONFIGS = {
     'default': {
         'toolbar': [
-            ['heading', '|', 'bold', 'italic', 'link'],
-            ['bulletedList', 'numberedList', 'blockQuote'],
-            ['undo', 'redo'],
-            ['imageUpload'],
+            ['heading', '|', 'bold', 'italic', 'underline', 'strikethrough', 'code', 'subscript', 'superscript'],
+            ['link', 'blockQuote', 'highlight'],
+            ['bulletedList', 'numberedList', 'todoList'],
+            ['alignment', 'outdent', 'indent'],
+            ['insertTable', 'mediaEmbed', 'imageUpload', 'horizontalLine'],
+            ['undo', 'redo', 'removeFormat'],
+            ['codeBlock', 'sourceEditing'],
         ],
         'heading': {
             'options': [
@@ -100,10 +103,52 @@ CKEDITOR_5_CONFIGS = {
                 {'model': 'heading1', 'view': 'h1', 'title': 'Heading 1', 'class': 'ck-heading_heading1'},
                 {'model': 'heading2', 'view': 'h2', 'title': 'Heading 2', 'class': 'ck-heading_heading2'},
                 {'model': 'heading3', 'view': 'h3', 'title': 'Heading 3', 'class': 'ck-heading_heading3'},
+                {'model': 'heading4', 'view': 'h4', 'title': 'Heading 4', 'class': 'ck-heading_heading4'},
+            ]
+        },
+        'alignment': {
+            'options': ['left', 'center', 'right', 'justify']
+        },
+        'table': {
+            'contentToolbar': [
+                'tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties'
+            ]
+        },
+        'image': {
+            'toolbar': [
+                'imageTextAlternative', 'imageStyle:inline', 'imageStyle:block', 'imageStyle:side'
+            ]
+        },
+        'mediaEmbed': {
+            'previewsInData': True
+        },
+        'codeBlock': {
+            'languages': [
+                {'language': 'plaintext', 'label': 'Plain text'},
+                {'language': 'python', 'label': 'Python'},
+                {'language': 'javascript', 'label': 'JavaScript'},
+                {'language': 'html', 'label': 'HTML'},
+                {'language': 'css', 'label': 'CSS'},
+                {'language': 'bash', 'label': 'Bash'},
+                {'language': 'json', 'label': 'JSON'},
+            ]
+        },
+        'language': 'en',
+        'placeholder': 'Start typing your content here...',
+        'removePlugins': ['Title'], 
+        'htmlSupport': {
+            'allow': [
+                {
+                    'name': /.*/,
+                    'attributes': True,
+                    'classes': True,
+                    'styles': True
+                }
             ]
         }
     }
 }
+
 
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
