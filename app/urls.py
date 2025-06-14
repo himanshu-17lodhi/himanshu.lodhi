ae0b452f51bc8bed9ae1d44d6cc7de1664b09d4a
Himanshu Lodhi <125793095+himanshu-17lodhi@users.noreply.github.com>
2025-06-14 12:42:26 +0530

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -7,12 +7,10 @@ from .views import (
     search,
     handler404,
 )
-
 from django.conf import settings
 from django.conf.urls.static import static
 
 handler404 = handler404
-
 urlpatterns = [
     path(settings.DJANGO_ADMIN_URL, admin.site.urls),
     path('', homePage, name='homePage'),
@@ -22,6 +20,5 @@ urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
     path('ckeditor5/', include('django_ckeditor_5.urls')),
 ]
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
