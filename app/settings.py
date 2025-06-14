33b879ee738cca83f9fffb73a119431417f0e948
Himanshu Lodhi <125793095+himanshu-17lodhi@users.noreply.github.com>
2025-06-14 11:10:26 +0530

    added CKEDITOR_5_CONFIGS

--- app/settings.py
+++ app/settings.py
@@ -5,26 +5,20 @@ import cloudinary
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Security
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
-# Admin URL
 DJANGO_ADMIN_URL = config('DJANGO_ADMIN_URL', default='admin/')
 
-# CORS
 CORS_ORIGIN_ALLOW_ALL = config('CORS_ORIGIN_ALLOW_ALL', default=True, cast=bool)
 CORS_ALLOW_CREDENTIALS = True
 
-# Authentication URLs
 LOGIN_URL = '/dashboard/login/'
 LOGOUT_URL = '/dashboard/logout/'
 
-# Auto primary key field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Installed apps
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -41,7 +35,6 @@ INSTALLED_APPS = [
     'rest_framework',
 ]
 
-# Middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
@@ -53,11 +46,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# URL Configuration
 ROOT_URLCONF = 'app.urls'
 WSGI_APPLICATION = 'app.wsgi.application'
 
-# Templates
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -77,7 +68,6 @@ TEMPLATES = [
     },
 ]
 
-# Database
 if not DEBUG:
     DATABASES = {
         'default': dj_database_url.config(default=config('DATABASE_URL'))
@@ -89,27 +79,43 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
-# Cloudinary Configuration
+    
 cloudinary.config( 
     cloud_name = config("CLOUDINARY_CLOUD_NAME"), 
     api_key = config("CLOUDINARY_API_KEY"), 
     api_secret = config("CLOUDINARY_API_SECRET") 
 )
 
+CKEDITOR_5_CONFIGS = {
+    'default': {
+        'toolbar': [
+            ['heading', '|', 'bold', 'italic', 'link'],
+            ['bulletedList', 'numberedList', 'blockQuote'],
+            ['undo', 'redo'],
+            ['imageUpload'],
+        ],
+        'heading': {
+            'options': [
+                {'model': 'paragraph', 'title': 'Paragraph', 'class': 'ck-heading_paragraph'},
+                {'model': 'heading1', 'view': 'h1', 'title': 'Heading 1', 'class': 'ck-heading_heading1'},
+                {'model': 'heading2', 'view': 'h2', 'title': 'Heading 2', 'class': 'ck-heading_heading2'},
+                {'model': 'heading3', 'view': 'h3', 'title': 'Heading 3', 'class': 'ck-heading_heading3'},
+            ]
+        }
+    }
+}
+
+
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-# Static files
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-# Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Email settings
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
@@ -117,7 +123,6 @@ EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
-# Localization
 LANGUAGE_CODE = config('LANGUAGE_CODE', default='en-us')
 TIME_ZONE = 'UTC'
 USE_I18N = True
