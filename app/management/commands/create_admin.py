05d793852ae079d4b93c3fcd3fa0d18b5fa41b12
Himanshu Lodhi <125793095+himanshu-17lodhi@users.noreply.github.com>
2025-07-21 12:41:11 +0530

    superuser managed from .env

--- app/management/commands/create_admin.py
+++ app/management/commands/create_admin.py
@@ -1,14 +1,22 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
+import os
 
 class Command(BaseCommand):
     help = 'Creates a superuser if it does not exist'
 
     def handle(self, *args, **kwargs):
         User = get_user_model()
-        username = "" 
-        email = ""   
-        password = "" 
+        username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+        email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
+        if not username or not email or not password:
+            self.stdout.write(self.style.ERROR(
+                'Please set DJANGO_SUPERUSER_USERNAME, DJANGO_SUPERUSER_EMAIL, and DJANGO_SUPERUSER_PASSWORD environment variables.'
+            ))
+            return
+
         if not User.objects.filter(username=username).exists():
             User.objects.create_superuser(username=username, email=email, password=password)
             self.stdout.write(self.style.SUCCESS('Superuser created!'))
