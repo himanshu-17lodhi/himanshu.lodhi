66d054c24e9eada8f9e5b8eb97171729a5c9e906
Himanshu <lodhihimanshu17@gmail.com>
2026-01-10 17:58:46 +0530

    Switch to Transaction Pooler with conn_max_age=0

--- app/settings.py
+++ app/settings.py
@@ -77,7 +77,7 @@ if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.config(
             default=os.environ.get('DATABASE_URL'),
-            conn_max_age=600,
+            conn_max_age=0,
             conn_health_checks=True,
         )
     }
