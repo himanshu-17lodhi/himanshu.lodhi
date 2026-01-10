fd3204916c487378a6cd14eb1e755f321e49ec50
Himanshu <lodhihimanshu17@gmail.com>
2026-01-11 00:16:09 +0530

    work: update dashboard logic and add deployment config
    c
    ':wq

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,16 +1,12 @@
 from django import forms
 from info.models import Information, Project
 
-
-
 class EditProfileForm(forms.ModelForm):
-	
 	class Meta:
 		model = Information
 		exclude = ('born_date', 'address', 'cv')
 
 class CreateProjectForm(forms.ModelForm):
-
 	class Meta:
 		model = Project
 		exclude = ('Slug',)
--- dashboard/urls.py
+++ dashboard/urls.py
@@ -15,21 +15,14 @@ from .views import (
 
 app_name = 'dashboard'
 urlpatterns = [
-
     path('', dashboard, name='dashboard'),
-
     path('profile/', profile, name='profile'),
     path('profile/edit/', profile_edit, name='profile_edit'),
-
     path('messages', messages, name='messages'),
     path('messages/api/', messages_api, name='messages_api'),
-
     path('projects', projects, name='projects'),
     path('projects/api/', projects_api, name='projects_api'),
-
     path('education/', EducationView.as_view(), name='education'),
-    # path('education/', EducationView.as_view(), name='one_education'),
-
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
